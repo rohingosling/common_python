dfcd355210e7b8d67182a22f49eb1a68ae88867b
Rohin Gosling <rohingosling@gmail.com>
2017-07-10 21:20:12 +0200

    Auto sync code.

--- application/numerai/numerai_3/main.py
+++ application/numerai/numerai_3/main.py
@@ -47,5 +47,5 @@ def main():
 
 if __name__ == "__main__":
 
-     main()# -*- coding: utf-8 -*-
+     main()
 
---
+++ git_sync.bat
@@ -0,0 +1,8 @@
+git status
+git add *
+git status
+git commit -m "Auto sync code."
+git status
+git push origin master
+git status
+git branch
