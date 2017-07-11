27600821f4d1e885c3abd530ea264a7abb3f8250
Rohin Gosling <rohingosling@gmail.com>
2017-07-11 22:27:19 +0200

    Auto sync code.

--- git_sync.bat
+++ git_sync.bat
@@ -1,8 +1,7 @@
 cls
 echo off
-sound 10000 50
-sound 10000 50
-sound 10000 200
+sound 100 50
+sound 200 100
 
 sound 12000 50
 echo -
@@ -52,9 +51,8 @@ echo - Active branch.
 echo -
 git branch
 sound 12000 50
-sound 100 50
-sound 100 50
-sound 200 200
+sound 200 50
+sound 100 100
 echo on
 
 
