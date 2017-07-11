583da98b086e2d703f2d456ac58e4f10dd84a395
Rohin Gosling <rohingosling@gmail.com>
2017-07-11 22:19:53 +0200

    Auto sync code.

--- git_sync.bat
+++ git_sync.bat
@@ -1,17 +1,29 @@
 cls
 echo off
 sound 12000 50
+
+echo
 echo GIT Status.
+echo
 git status
+
 sound 12000 50
+echo
 echo Adding files.
+echo
 git add *
+
 sound 12000 50
 echo GIT Status.
+echo
 git status
+echo
+
 sound 12000 50
 echo Commiting changes.
+echo
 git commit -m "Auto sync code."
+
 sound 12000 50
 echo GIT Status.
 git status
